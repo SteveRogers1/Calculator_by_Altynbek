5a10456e9a518fdb4696bfb15eab89f7fe7db4ba
Алтынбек Турсункулов <aaa.altyn99@mail.ru>
2022-06-20 11:54:35 +0600

    Add project files.

---
+++ Calculator_by_Altynbek/Program.cs
@@ -0,0 +1,100 @@
+﻿
+bool flag;
+double first_number = 0;
+double second_number = 0;
+double answer = 0;
+char? action = null;
+char? action2 = null;
+
+do
+{
+    do
+    {
+        try
+        {
+            Console.Write("Enter first number: ");
+            first_number = Convert.ToDouble(Console.ReadLine());
+            flag = true;
+        }
+        catch
+        {
+            flag = false;
+            Console.WriteLine("Error!");
+        }
+    }
+    while (flag != true);
+    do
+    {
+        try
+        {
+            Console.WriteLine("Select an action : ");
+            action = Convert.ToChar(Console.ReadLine());
+            if (action == '*' || action == '+' || action == '/' || action == '-')
+            {
+                flag = false;
+            }
+        }
+        catch
+        {
+            flag = true;
+            Console.WriteLine("Error!");
+        }
+    }
+    while (flag != false);
+
+    do
+    {
+        try
+        {
+            Console.Write("Enter second number: ");
+            second_number = Convert.ToDouble(Console.ReadLine());
+            flag = true;
+        }
+        catch
+        {
+            flag = false;
+            Console.WriteLine("Error!");
+        }
+    }
+    while (flag != true);
+
+
+
+    switch (action)
+    {
+        case '*':
+            answer = first_number * second_number;
+            Console.WriteLine("Ответ: " + answer);
+            break;
+        case '+':
+            answer = first_number + second_number;
+            Console.WriteLine("Ответ: " + answer);
+            break;
+        case '/':
+            answer = first_number / second_number;
+            Console.WriteLine("Ответ: " + answer);
+            break;
+        case '-':
+            answer = first_number - second_number;
+            Console.WriteLine("Ответ: " + answer);
+            break;
+    }
+    do
+    {
+        try
+        {
+            Console.WriteLine("start again? Y/N : ");
+            action2 = Convert.ToChar(Console.ReadLine());
+                flag = true;
+            
+        }
+        catch
+        {
+            flag = false;
+            Console.WriteLine("Error!");
+        }
+    }
+    while (flag != true);
+}
+while (action2 == 'Y');
+
